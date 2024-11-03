50ff3057eddce33764d36f00d493682872f850cc
Marek Štec <mar.stec@seznam.cz>
2024-11-03 10:34:16 +0100

    enable uart4

--- extra_script.sh
+++ extra_script.sh
@@ -18,3 +18,14 @@ if ! grep -q "^event_scheduler\s*=\s*ON" "$CONFIG_FILE"; then
 fi
 
 log_message "Mysql configuration finished"
+
+
+# enable uart4 for osdp communication
+uart_enabled=$(grep "^dtoverlay=uart4" /boot/config.txt)
+if [ -z $uart_enabled ]; then
+    echo "dtoverlay=uart4" | sudo tee -a /boot/config.txt
+    log_message "UART4 overlay enabled. Rebooting system."
+    sudo reboot
+else
+    log_message "UART4 overlay is already enabled."
+fi
